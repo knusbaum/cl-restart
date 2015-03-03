c15f9ce3ce5f5b315e9609f7392611e5b4f32a09
Kyle Nusbaum <knusbaum@yahoo-inc.com>
2015-03-03 14:15:02 -0600

    Adding scm to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject org.clojars.knusbaum/restarts "0.1.0-SNAPSHOT"
+(defproject org.clojars.knusbaum/restarts "0.0.1"
   :description "A simple Common Lisp style restart library for Clojure."
   :url "http://www.github.com/knusbaum/cl-restart"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :scm {:name "git"
+        :url "https://github.com/knusbaum/cl-restart.git"})
